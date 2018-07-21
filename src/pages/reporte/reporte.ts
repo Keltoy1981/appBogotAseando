dc65e04da32e6187729656d55427eb385d9c7620
Miguel Higuera <higueraprieto1981@gmail.com>
2018-07-21 15:00:33 -0500

    implementación de foto-reporte

--- src/pages/reporte/reporte.ts
+++ src/pages/reporte/reporte.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ReportePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera'
 
 @IonicPage()
 @Component({
@@ -15,7 +9,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ReportePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	public fotos:any;
+	public base64Image:string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera, private alertCtrl:AlertController) {
+  }
+
+  ngOnInit(){
+  	this.fotos = [];
+  }
+
+  tomarFoto(){
+  	const options: CameraOptions = {
+	  quality: 50,
+	  destinationType: this.camera.DestinationType.FILE_URI,
+	  encodingType: this.camera.EncodingType.JPEG,
+	  mediaType: this.camera.MediaType.PICTURE
+	}
+
+	this.camera.getPicture(options).then((imageData) => {
+	 // imageData is either a base64 encoded string or a file URI
+	 // If it's base64 (DATA_URL):
+	 this.base64Image = 'data:image/jpeg;base64,' + imageData;
+	 this.fotos.push(this.base64Image);
+	 this.fotos.reverse();
+	}, (err) => {
+	 // Handle error
+	});
+  }
+
+  borrarFoto(index){
+  	 const confirm = this.alertCtrl.create({
+      title: '¿Desea eliminar la foto?',
+      message: '',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            this.fotos.splice(index,1);
+          }
+        }
+      ]
+    });
+    confirm.present();
+
+  	
   }
 
   ionViewDidLoad() {
